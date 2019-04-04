9c7436c27449ea0a6c8b3a3c1b7a874d0ddaaba6
haunted-warbler <nareshkumar7593@gmail.com>
2019-04-04 12:01:29 -0700

    Bubble Sort Problems

--- src/SortingProblems/BubbleSort.java
+++ src/SortingProblems/BubbleSort.java
@@ -1,4 +1,26 @@
 package SortingProblems;
 
 public class BubbleSort {
+
+  public static void main(String[] args) {
+
+    int[] arr = {44, 11, 18, 33, 21, 67, 99, 103, 11, 42, 61, 88};
+    int temp = 0;
+    boolean flag = true;
+    while(flag == true) {
+      flag = false;
+      for (int i = 1; i < arr.length; i++) {
+        if (arr[i - 1] > arr[i]) {
+          temp = arr[i];
+          arr[i] = arr[i - 1];
+          arr[i - 1] = temp;
+          flag = true;
+        }
+      }
+    }
+
+    for(int sortedArr : arr)
+      System.out.print(sortedArr+ ", ");
+
+  }
 }
